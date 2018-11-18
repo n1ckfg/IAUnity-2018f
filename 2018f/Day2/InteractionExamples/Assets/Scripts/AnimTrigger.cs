d8988d576f41e0122d4b7658a74e03b597047925
Nick Fox-Gieg <nick@fox-gieg.com>
2018-11-17 19:28:39 -0500

    new model

--- 2018f/Day2/InteractionExamples/Assets/Scripts/AnimTrigger.cs
+++ 2018f/Day2/InteractionExamples/Assets/Scripts/AnimTrigger.cs
@@ -7,17 +7,12 @@ public class AnimTrigger : MonoBehaviour {
 	public Animator anim;
 	public bool trigger = false;
 	public float delay = 0;
-	
-	// Use this for initialization
-	void Start () {
-		//
-	}
-	
-	// Update is called once per frame
+
 	void Update () {
 		if(trigger && Time.time > delay){
 			anim.Play(0);
 			trigger = false;
 		}
 	}
+
 }
